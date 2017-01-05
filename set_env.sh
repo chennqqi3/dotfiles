610b31164b7f44bd4ffc66f4d21bb6a901a553b2
luoziyihao <luoziyihao@gmail.com>
2017-01-05 17:40:44 +0800

    com

--- set_env.sh
+++ set_env.sh
@@ -6,6 +6,8 @@
 # Created Time: 2016-05-19
 #########################################################################
 
+# for own purge server
+
 sudo apt-get install zsh
 sudo usermod -s /bin/zsh `who|awk '{print($1)}'`
 sudo apt-get install tmux
@@ -19,12 +21,12 @@ oh_my_zsh_dest=$MY_HOME"/.oh-my-zsh"
 dircolors_solarized=$DOTFILES"/dircolors_solarized"
 dircolors_solarized_dest=$MY_HOME"/.dir_colors"
 
-if [ -d "${dircolors_solarized}" ]; then
-    echo -e "\033[34m zsh cfg archive: ${dircolors_solarized} \033[0m"
-else 
-    echo -e "\033[31m zsh cfg archive '${dircolors_solarized}' not exists, just git clone it \033[0m"
-    git clone https://github.com/seebi/dircolors-solarized.git ${dircolors_solarized}
-fi
+# if [ -d "${dircolors_solarized}" ]; then
+    # echo -e "\033[34m zsh cfg archive: ${dircolors_solarized} \033[0m"
+# else 
+    # echo -e "\033[31m zsh cfg archive '${dircolors_solarized}' not exists, just git clone it \033[0m"
+    # git clone https://github.com/seebi/dircolors-solarized.git ${dircolors_solarized}
+# fi
 
 if [ -d "${oh_my_zsh}" ]; then
     echo -e "\033[34m zsh cfg archive: ${oh_my_zsh} \033[0m"
@@ -33,21 +35,21 @@ else
     git clone git://github.com/robbyrussell/oh-my-zsh.git ${oh_my_zsh}
 fi
 
-if [ -d "${gnome_solarized}" ]; then
-    echo -e "\033[34m zsh cfg archive: ${gnome_solarized} \033[0m"
-else 
-    echo -e "\033[31m zsh cfg archive '${gnome_solarized}' not exists, just git clone it \033[0m"
-    git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git ${gnome_solarized}
-fi
+# if [ -d "${gnome_solarized}" ]; then
+    # echo -e "\033[34m zsh cfg archive: ${gnome_solarized} \033[0m"
+# else 
+    # echo -e "\033[31m zsh cfg archive '${gnome_solarized}' not exists, just git clone it \033[0m"
+    # git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git ${gnome_solarized}
+# fi
 
-# set origin background
-dconf reset -f /org/gnome/terminal/legacy/profiles:/
-# install  gnome_solarized
-${gnome_solarized}"/install.sh"
+# # set origin background
+# dconf reset -f /org/gnome/terminal/legacy/profiles:/
+# # install  gnome_solarized
+# ${gnome_solarized}"/install.sh"
 
 # set  dircolors
 # mk dircolorsdb source link
-ln -fs ${dircolors_solarized}"/dircolors.ansi-light" ${dircolors_solarized_dest}
+# ln -fs ${dircolors_solarized}"/dircolors.ansi-light" ${dircolors_solarized_dest}
 
 ln -fs ${DOTFILES}"/.bashrc" ${MY_HOME}"/.bashrc"
 ln -fs ${DOTFILES}"/.tmux.conf" ${MY_HOME}"/.tmux.conf"
@@ -59,4 +61,3 @@ ln -fs ${oh_my_zsh} ${oh_my_zsh_dest}
 
 #ok
 echo "ok"
-sudo ln -fs  ${DOTFILES}"/stth.sh" "/usr/local/bin/stth"
