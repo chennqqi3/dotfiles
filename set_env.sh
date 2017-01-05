db64341dd798da3f1c2d3fbc0fdf188cf50d968a
luoziyihao <luoziyihao@gmail.com>
2017-01-05 17:35:13 +0800

    init public

--- set_env.sh
+++ set_env.sh
@@ -6,6 +6,9 @@
 # Created Time: 2016-05-19
 #########################################################################
 
+sudo apt-get install zsh
+sudo apt-get install tmux
+
 MY_HOME=${HOME}
 DOTFILES=${MY_HOME}"/dotfiles"
 # 在对目录做阮链接的时候也不要用 '/', 尤其是目的的目录不要接'/'
@@ -15,12 +18,12 @@ oh_my_zsh_dest=$MY_HOME"/.oh-my-zsh"
 dircolors_solarized=$DOTFILES"/dircolors_solarized"
 dircolors_solarized_dest=$MY_HOME"/.dir_colors"
 
-if [ -d "${dircolors_solarized}" ]; then
-    echo -e "\033[34m zsh cfg archive: ${dircolors_solarized} \033[0m"
-else 
-    echo -e "\033[31m zsh cfg archive '${dircolors_solarized}' not exists, just git clone it \033[0m"
-    git clone https://github.com/seebi/dircolors-solarized.git ${dircolors_solarized}
-fi
+# if [ -d "${dircolors_solarized}" ]; then
+    # echo -e "\033[34m zsh cfg archive: ${dircolors_solarized} \033[0m"
+# else 
+    # echo -e "\033[31m zsh cfg archive '${dircolors_solarized}' not exists, just git clone it \033[0m"
+    # git clone https://github.com/seebi/dircolors-solarized.git ${dircolors_solarized}
+# fi
 
 if [ -d "${oh_my_zsh}" ]; then
     echo -e "\033[34m zsh cfg archive: ${oh_my_zsh} \033[0m"
@@ -29,28 +32,28 @@ else
     git clone git://github.com/robbyrussell/oh-my-zsh.git ${oh_my_zsh}
 fi
 
-if [ -d "${gnome_solarized}" ]; then
-    echo -e "\033[34m zsh cfg archive: ${gnome_solarized} \033[0m"
-else 
-    echo -e "\033[31m zsh cfg archive '${gnome_solarized}' not exists, just git clone it \033[0m"
-    git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git ${gnome_solarized}
-fi
+# if [ -d "${gnome_solarized}" ]; then
+    # echo -e "\033[34m zsh cfg archive: ${gnome_solarized} \033[0m"
+# else 
+    # echo -e "\033[31m zsh cfg archive '${gnome_solarized}' not exists, just git clone it \033[0m"
+    # git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git ${gnome_solarized}
+# fi
 
 # set origin background
-dconf reset -f /org/gnome/terminal/legacy/profiles:/
+# dconf reset -f /org/gnome/terminal/legacy/profiles:/
 # install  gnome_solarized
-${gnome_solarized}"/install.sh"
+# ${gnome_solarized}"/install.sh"
 
 # set  dircolors
 # mk dircolorsdb source link
-ln -fs ${dircolors_solarized}"/dircolors.ansi-light" ${dircolors_solarized_dest}
+# ln -fs ${dircolors_solarized}"/dircolors.ansi-light" ${dircolors_solarized_dest}
 
-ln -fs ${DOTFILES}"/.bashrc" ${MY_HOME}"/.bashrc"
+# ln -fs ${DOTFILES}"/.bashrc" ${MY_HOME}"/.bashrc"
 ln -fs ${DOTFILES}"/.tmux.conf" ${MY_HOME}"/.tmux.conf"
 ln -fs ${DOTFILES}"/.vimrc" ${MY_HOME}"/.vimrc"
 ln -fs ${DOTFILES}"/.ycm_extra_conf.py" ${MY_HOME}"/.ycm_extra_conf.py"
 ln -fs ${DOTFILES}"/.zshrc" ${MY_HOME}"/.zshrc"
-ln -fs ${DOTFILES}"/.npmrc" ${MY_HOME}"/.npmrc"
+# ln -fs ${DOTFILES}"/.npmrc" ${MY_HOME}"/.npmrc"
 ln -fs ${oh_my_zsh} ${oh_my_zsh_dest}
 
 #ok
